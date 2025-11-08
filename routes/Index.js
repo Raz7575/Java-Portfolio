5f860b8009ee66ef4792a86678668edfc0271ff6
Raz7575 <reshad.safi@ontariotechu.net>
2025-11-07 20:36:02 -0500

    Initial commit - portfolio website with blogs page

--- routes/Index.js
+++ routes/Index.js
@@ -2,22 +2,22 @@
 var express = require('express');
 var router = express.Router();
 
-// Home page
+
 router.get('/', function(req, res, next) {
   res.render('home', { title: 'Home' });
 });
 
-// About page
+
 router.get('/about', function(req, res, next) {
   res.render('about', { title: 'About Me' });
 });
 
-// Projects page
-router.get('/projects', function(req, res, next) {
-  res.render('projects', { title: 'Projects' });
+router.get('/blogs', function(req, res, next) {
+  res.render('blogs', { title: 'Blogs' });
 });
 
-// Contact page
+
+
 router.get('/contact', function(req, res, next) {
   res.render('contact', { title: 'Contact Me' });
 });
